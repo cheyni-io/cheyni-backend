ac27bb34c24df3950d4bf9a727a62f0fd9b285b1
Gunichi <gunichi297@gmail.com>
2024-01-04 14:37:05 -0300

    fix: update configs

--- src/config/database/data-source-config.ts
+++ src/config/database/data-source-config.ts
@@ -1,24 +1,3 @@
-// import { DataSource } from 'typeorm';
-// import { join } from 'path';
-
-// import { UserEntity } from 'src/entities/user.entity';
-
-// export const AppDataSource = (database: string) =>
-//   new DataSource({
-//     name: database,
-//     type: 'postgres',
-//     host: 'roundhouse.proxy.rlwy.net',
-//     port: 49275,
-//     username: 'postgres',
-//     password: '1dAcEEc5DAA*3EC4g63b*3C2CBAAgdBd',
-//     database: 'railway',
-//     synchronize: true,
-//     migrationsRun: true,
-//     logging: true,
-//     entities: [UserEntity],
-//     migrations: [join(__dirname, '..', '..', '/migrations')],
-//     migrationsTableName: 'migrations',
-//   });
 import { DataSource } from 'typeorm';
 import { join } from 'path';
 
@@ -29,11 +8,11 @@ export const AppDataSource = (database: string) =>
   new DataSource({
     name: database,
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: 'roundhouse.proxy.rlwy.net',
+    port: 49275,
     username: 'postgres',
-    password: '1234',
-    database: 'postgres',
+    password: '1dAcEEc5DAA*3EC4g63b*3C2CBAAgdBd',
+    database: 'railway',
     synchronize: true,
     migrationsRun: true,
     logging: true,
@@ -41,3 +20,25 @@ export const AppDataSource = (database: string) =>
     migrations: [join(__dirname, '..', '..', '/migrations')],
     migrationsTableName: 'migrations',
   });
+// import { DataSource } from 'typeorm';
+// import { join } from 'path';
+
+// import { UserEntity } from 'src/entities/user.entity';
+// import { UploadEntity } from 'src/entities/upload.entity';
+
+// export const AppDataSource = (database: string) =>
+//   new DataSource({
+//     name: database,
+//     type: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     username: 'postgres',
+//     password: '1234',
+//     database: 'postgres',
+//     synchronize: true,
+//     migrationsRun: true,
+//     logging: true,
+//     entities: [UserEntity, UploadEntity],
+//     migrations: [join(__dirname, '..', '..', '/migrations')],
+//     migrationsTableName: 'migrations',
+//   });
