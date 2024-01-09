dac70b053d78eff9ed415dfb89f755dd75f68c83
Gunichi <gunichi297@gmail.com>
2024-01-09 10:37:03 -0300

    fix: uploads

--- src/config/database/data-source-config.ts
+++ src/config/database/data-source-config.ts
@@ -5,37 +5,15 @@ import { UserEntity } from 'src/entities/user.entity';
 import { UploadEntity } from 'src/entities/upload.entity';
 import { GenreEntity } from 'src/entities/genres.entity';
 
-// export const AppDataSource = (database: string) =>
-//   new DataSource({
-//     name: database,
-//     type: 'postgres',
-//     host: 'roundhouse.proxy.rlwy.net',
-//     port: 49275,
-//     username: 'postgres',
-//     password: '1dAcEEc5DAA*3EC4g63b*3C2CBAAgdBd',
-//     database: 'railway',
-//     synchronize: true,
-//     migrationsRun: true,
-//     logging: true,
-//     entities: [UserEntity, UploadEntity, GenreEntity],
-//     migrations: [join(__dirname, '..', '..', '/migrations')],
-//     migrationsTableName: 'migrations',
-//   });
-// import { DataSource } from 'typeorm';
-// import { join } from 'path';
-
-// import { UserEntity } from 'src/entities/user.entity';
-// import { UploadEntity } from 'src/entities/upload.entity';
-
 export const AppDataSource = (database: string) =>
   new DataSource({
     name: database,
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: 'roundhouse.proxy.rlwy.net',
+    port: 49275,
     username: 'postgres',
-    password: '1234',
-    database: 'postgres',
+    password: '1dAcEEc5DAA*3EC4g63b*3C2CBAAgdBd',
+    database: 'railway',
     synchronize: true,
     migrationsRun: true,
     logging: true,
@@ -43,3 +21,25 @@ export const AppDataSource = (database: string) =>
     migrations: [join(__dirname, '..', '..', '/migrations')],
     migrationsTableName: 'migrations',
   });
+// import { DataSource } from 'typeorm';
+// import { join } from 'path';
+
+// import { UserEntity } from 'src/entities/user.entity';
+// import { UploadEntity } from 'src/entities/upload.entity';
+
+// export const AppDataSource = (database: string) =>
+//   new DataSource({
+//     name: database,
+//     type: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     username: 'postgres',
+//     password: '1234',
+//     database: 'postgres',
+//     synchronize: true,
+//     migrationsRun: true,
+//     logging: true,
+//     entities: [UserEntity, UploadEntity, GenreEntity],
+//     migrations: [join(__dirname, '..', '..', '/migrations')],
+//     migrationsTableName: 'migrations',
+//   });
--- src/modules/upload/upload.repository.ts
+++ src/modules/upload/upload.repository.ts
@@ -40,6 +40,7 @@ export class UploadRepository extends Repository<UploadEntity> {
         'description',
         'createdAt',
         'updatedAt',
+        'thumbnail',
       ],
     });
   }
