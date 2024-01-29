a3c41a3ec5e8f9212c26309c69cc33d0b0c03a08
Gunichi <gunichi297@gmail.com>
2024-01-29 15:10:54 -0300

    fix configs

--- src/config/database/data-source-config.ts
+++ src/config/database/data-source-config.ts
@@ -8,29 +8,14 @@ import { NFTokenEntity } from 'src/entities/nftoken.entity';
 import { NFTokenAndUserEntity } from 'src/entities/nfTokenAndUser.entity';
 
 export const AppDataSource = (database: string) =>
-  // new DataSource({
-  //   name: database,
-  //   type: 'postgres',
-  //   host: 'roundhouse.proxy.rlwy.net',
-  //   port: 49275,
-  //   username: 'postgres',
-  //   password: '1dAcEEc5DAA*3EC4g63b*3C2CBAAgdBd',
-  //   database: 'railway',
-  //   synchronize: true,
-  //   migrationsRun: true,
-  //   logging: true,
-  //   entities: [UserEntity, UploadEntity, GenreEntity],
-  //   migrations: [join(__dirname, '..', '..', '/migrations')],
-  //   migrationsTableName: 'migrations',
-  // });
   new DataSource({
     name: database,
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: 'roundhouse.proxy.rlwy.net',
+    port: 49275,
     username: 'postgres',
-    password: '1234',
-    database: 'postgres',
+    password: '1dAcEEc5DAA*3EC4g63b*3C2CBAAgdBd',
+    database: 'railway',
     synchronize: true,
     migrationsRun: true,
     logging: true,
@@ -44,3 +29,24 @@ export const AppDataSource = (database: string) =>
     migrations: [join(__dirname, '..', '..', '/migrations')],
     migrationsTableName: 'migrations',
   });
+// new DataSource({
+//   name: database,
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: '1234',
+//   database: 'postgres',
+//   synchronize: true,
+//   migrationsRun: true,
+//   logging: true,
+//   entities: [
+//     UserEntity,
+//     UploadEntity,
+//     GenreEntity,
+//     NFTokenEntity,
+//     NFTokenAndUserEntity,
+//   ],
+//   migrations: [join(__dirname, '..', '..', '/migrations')],
+//   migrationsTableName: 'migrations',
+// });
